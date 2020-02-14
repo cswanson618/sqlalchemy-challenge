02d8ae3a0e19eda05dd78a19034e53941637b279
cswanson618 <54300477+cswanson618@users.noreply.github.com>
2020-02-13 21:12:06 -0600

    Formatted code and made tweaks

--- climateapp.py
+++ climateapp.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import datetime as dt
+from datetime import datetime
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -25,7 +26,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def SplashPage():
-    return "Welcome to the Climate App."
+    return (
+        f"Welcome to The Climate App<br/>"
+        f"<br/>"
+        f"Available Routes:<br/>"
+        f"<br/>"
+        f"/api/v1.0/precipitation which returns precipitation in Hawaii by date from 2016-08-23 to 2017-08-23<br/>"
+        f"/api/v1.0/stations which returns unique measuring stations in Hawaii and how many measurements they recorded<br/>"
+        f"/api/v1.0/temps/START which returns temperature measures from the most active station beginning at <start> date*<br/>"
+        f"/api/v1.0/temps/START/END which returns temperature measures from the most active station beginning at START date* and ending at END date*<br/>"
+        f"<br/>"
+        f"<b>*IMPORTANT NOTE: Dates MUST be formatted YYYY-MM-DD</b>"
+    )
 
 
 precip_data = (
@@ -69,6 +81,43 @@ stations_df = pd.DataFrame(active_stations, columns=["Station", "Number of Measu
 def StationsQuery():
     stations_json = stations_df.to_json(orient="records")
     return stations_json
+
+
+temp_active = (
+    session.query(Measurement.station, Measurement.tobs, Measurement.date)
+    .filter(Measurement.station == "USC00519281")
+    .filter(
+        Measurement.date >= dt.date(2016, 8, 23),
+        Measurement.date <= dt.date(2017, 8, 23),
+    )
+    .all()
+)
+temp_active_df_unclean = pd.DataFrame(temp_active)
+temp_active_date = temp_active_df_unclean["date"]
+temp_active_date_clean = pd.DataFrame(pd.to_datetime(temp_active_date))
+temp_active_df_unclean2 = temp_active_df_unclean
+temp_active_df_unclean3 = temp_active_df_unclean2.drop(columns="date")
+temp_active_df = temp_active_df_unclean3.join(temp_active_date_clean)
+
+
+@app.route("/api/v1.0/temps/<start>")
+def TempQuery(start):
+    date_list = []
+    for index, row in temp_active_df.iterrows():
+        if row["date"] >= datetime.strptime(start, "%Y-%m-%d"):
+            date_list.append((row["date"], row["tobs"]))
+    return jsonify(date_list)
+
+
+@app.route("/api/v1.0/temps/<start>/<end>")
+def TempQuerySE(start, end):
+    date_list = []
+    for index, row in temp_active_df.iterrows():
+        if row["date"] >= datetime.strptime(start, "%Y-%m-%d") and row[
+            "date"
+        ] <= datetime.strptime(end, "%Y-%m-%d"):
+            date_list.append((row["date"], row["tobs"]))
+    return jsonify(date_list)
 
 
 if __name__ == "__main__":
